3029eeb9bdbbb3a41a6047b0ac6f31bf09d58800
Rushikesh Korgaonkar <rushikesh.korgaonkar@gmail.com>
2020-03-17 20:01:06 +0700

    Generate accessToken added

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
+
+app.use(express.json());
+const userdetails = [
+  {
+    username: "rushi",
+    password: "test123"
+  },
+  { username: "korgaonkar", password: "test908" }
+];
+
+app.get("/users", (req, res) => {
+  res.json(userdetails);
+});
+
+app.post("/login", (req, res) => {
+  //1.Authenticate user
+
+  //2.Token
+  const username = req.body.username;
+  const user = { name: username };
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken: accessToken });
+});
 
 app.listen(3000);
