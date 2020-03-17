db2ecc52001c572c2e673c463e510811a4db4eae
Rushikesh Korgaonkar <rushikesh.korgaonkar@gmail.com>
2020-03-17 22:23:33 +0700

    Authentication and Authorization added

--- server.js
+++ server.js
@@ -16,14 +16,27 @@ app.get("/users", (req, res) => {
   res.json(userdetails);
 });
 
+app.get("/posts", authenticateToken, (req, res) => {
+  res.json(userdetails.filter(user => user.username === req.user.name));
+});
+
 app.post("/login", (req, res) => {
   //1.Authenticate user
-
-  //2.Token
   const username = req.body.username;
   const user = { name: username };
   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
   res.json({ accessToken: accessToken });
 });
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 
 app.listen(3000);
