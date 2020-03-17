9cfd1d3760c0ad9515832c23d231edfdc254d4b7
Rushikesh Korgaonkar <rushikesh.korgaonkar@gmail.com>
2020-03-17 23:13:45 +0700

    Token Refresh and Delete added

--- server.js
+++ server.js
@@ -20,14 +20,6 @@ app.get("/posts", authenticateToken, (req, res) => {
   res.json(userdetails.filter(user => user.username === req.user.name));
 });
 
-app.post("/login", (req, res) => {
-  //1.Authenticate user
-  const username = req.body.username;
-  const user = { name: username };
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-  res.json({ accessToken: accessToken });
-});
-
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
